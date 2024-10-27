8528be00abc734d48254b43a2e41f62275046f91
wei047 <aabbcc123yun@gmail.com>
2024-10-27 14:21:26 +0800

    Create 1024-1.js

---
+++ 1024-1.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var age = 25;
+console.log("原始常數 age:", age);
+var name = "Alice";
+console.log("變數 name:", name);
+name = "Wei";
+console.log("更改後的變數name:", name);
+console.log("".concat(name, " age").concat(age, " "));
