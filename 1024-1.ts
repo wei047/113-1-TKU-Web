e993a18cde8f202ed21016c011fcc890ce72c3b1
wei047 <aabbcc123yun@gmail.com>
2024-10-27 14:21:31 +0800

    Create 1024-1.ts

---
+++ 1024-1.ts
@@ -0,0 +1,8 @@
+const age: number = 25;
+console.log("原始常數 age:", age);
+let name: string = "Alice";
+console.log("變數 name:", name);
+name = "Wei";
+console.log("更改後的變數name:", name);
+console.log(`${name} age${age} `);
+export {}; 
